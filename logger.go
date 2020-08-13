422b7d87f8257aa29f5cb10460f16a805dbb866e
mnv <mnvx@yandex.ru>
2020-08-14 00:26:01 +0300

    logger interface

--- logger.go
+++ logger.go
@@ -1,5 +1,12 @@
 package gorm_crud
 
 type LoggerInterface interface {
+	Debug(args ...interface{})
+	Debugf(format string, args ...interface{})
+	Info(args ...interface{})
+	Infof(format string, args ...interface{})
 	Error(args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(format string, args ...interface{})
 }
