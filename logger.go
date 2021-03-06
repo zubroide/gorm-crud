fbc2ebc54ade7f37c18e81d97989622e974d0901
mnv <mnvx@yandex.ru>
2021-03-06 23:49:22 +0300

    logger: warn

--- logger.go
+++ logger.go
@@ -5,6 +5,8 @@ type LoggerInterface interface {
 	Debugf(format string, args ...interface{})
 	Info(args ...interface{})
 	Infof(format string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(format string, args ...interface{})
 	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
 	Fatal(args ...interface{})
